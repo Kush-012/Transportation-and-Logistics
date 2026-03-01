20c6467d58f96a9649e207ffb4ab3eeb6b4cce39
Ahmad Hasssan <ahmadhassan478626@gmail.com>
2026-03-01 15:30:10 +0500

    Add: Health check endpoint with database status monitoring

--- b/routes/healthCheck/healthCheck.js
+++ b/routes/healthCheck/healthCheck.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const authmiddleware = require("../../middlewares/auth");
-const { authorizeRoles } = require("../../middlewares/userRole");
-const { checkHealth } = require("../../controllers/healthCheck/healthCheck")
+const { checkHealth } = require("../../controllers/healthCheck/healthCheck");
 
 
 // Health check endpoint
